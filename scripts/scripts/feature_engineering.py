450a4fe9fa6b2db0e8a3d27cf77e9544b81aa725
sravani-sony <esampallysravani@gmail.com>
2025-11-27 01:48:30 -0600

    Create feature_engineering.py

---
+++ scripts/scripts/feature_engineering.py
@@ -0,0 +1,12 @@
+def create_features(df):
+    """
+    Generates patient-level features from claims data:
+    - Total cost per patient
+    - Total number of claims
+    - Average claim cost
+    """
+    df['total_cost'] = df.groupby('patient_id')['cost'].transform('sum')
+    df['claim_count'] = df.groupby('patient_id')['claim_id'].transform('count')
+    df['avg_cost'] = df.groupby('patient_id')['cost'].transform('mean')
+
+    return df
